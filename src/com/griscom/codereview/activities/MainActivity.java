ad3f97469794a4282a4f7286fe4975a99067e9f5
Gris87 <Gris87@yandex.ru>
2014-04-18 11:13:00 +0400

    Action close added

--- src/com/griscom/codereview/activities/MainActivity.java
+++ src/com/griscom/codereview/activities/MainActivity.java
@@ -53,11 +53,18 @@ public class MainActivity extends ActionBarActivity
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id=item.getItemId();
-
-        if (id==R.id.action_settings)
+        switch(item.getItemId())
         {
-            return true;
+            case R.id.action_settings:
+            {
+                return true;
+            }
+
+            case R.id.action_close:
+            {
+                finish();
+                return true;
+            }
         }
 
         return super.onOptionsItemSelected(item);
