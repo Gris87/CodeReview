f1d668cad98133057433a22d8f66fb6d3c762aa4
Gris87 <Gris87@yandex.ru>
2014-05-07 23:14:11 +0400

    Old school

--- src/com/griscom/codereview/activities/ReviewActivity.java
+++ src/com/griscom/codereview/activities/ReviewActivity.java
@@ -1,18 +1,23 @@
 package com.griscom.codereview.activities;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 
 import com.griscom.codereview.R;
+import com.griscom.codereview.other.ApplicationExtras;
+import com.griscom.codereview.review.ReviewSurfaceView;
 
-public class ReviewActivity extends ActionBarActivity
+public class ReviewActivity extends FragmentActivity
 {
     public static final int RESULT_CLOSE=1;
 
@@ -33,7 +38,6 @@ public class ReviewActivity extends ActionBarActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_review, menu);
         return true;
     }
@@ -41,9 +45,6 @@ public class ReviewActivity extends ActionBarActivity
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         switch(item.getItemId())
         {
             case R.id.action_settings:
@@ -56,6 +57,9 @@ public class ReviewActivity extends ActionBarActivity
 
             case R.id.action_close:
             {
+                Intent data=new Intent();
+
+                setResult(RESULT_CLOSE, data);
                 finish();
                 return true;
             }
@@ -67,9 +71,11 @@ public class ReviewActivity extends ActionBarActivity
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment
+    public static class PlaceholderFragment extends Fragment implements OnTouchListener
     {
-        private ReviewActivity mActivity;
+        private ReviewActivity    mActivity;
+        private ReviewSurfaceView mContent;
+        private String            mFileName;
 
         public PlaceholderFragment()
         {
@@ -80,8 +86,55 @@ public class ReviewActivity extends ActionBarActivity
         {
             mActivity=(ReviewActivity)getActivity();
 
+            Intent intent=mActivity.getIntent();
+            mFileName=intent.getStringExtra(ApplicationExtras.OPEN_FILE);
+
             View rootView=inflater.inflate(R.layout.fragment_review, container, false);
+
+            mContent = (ReviewSurfaceView)rootView.findViewById(R.id.fullscreen_content);
+
+            mContent.setFileName(mFileName);
+            mContent.setOnTouchListener(this);
+
             return rootView;
+        }
+
+        @Override
+        public void onDestroy()
+        {
+            super.onDestroy();
+
+            mContent.onDestroy();
+        }
+
+        @Override
+        public void onPause()
+        {
+            super.onPause();
+
+            mContent.onPause();
+        }
+
+        @Override
+        public void onResume()
+        {
+            super.onResume();
+
+            mContent.onResume();
+        }
+
+        @Override
+        public void onConfigurationChanged(Configuration newConfig)
+        {
+            super.onConfigurationChanged(newConfig);
+
+            mContent.onConfigurationChanged(newConfig);
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event)
+        {
+            return mContent.onTouch(v, event);
         }
     }
 }
